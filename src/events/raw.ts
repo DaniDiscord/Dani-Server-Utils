db54655cc77302e23fe0796efcbc3063d2315ec3
Tecci <lotvome@hotmail.com>
2024-04-09 15:10:00 +0300

    anti-polls

---
+++ src/events/raw.ts
@@ -0,0 +1,60 @@
+import { CustomClient } from "lib/client";
+import { SettingsModel } from "models/Settings";
+
+export default async (client: CustomClient, packet: any): Promise<void> => {
+  const data = packet.d;
+
+  if (!data.poll) {
+    return;
+  }
+
+  const channel = await client.channels.fetch(data.channel_id);
+
+  if (!channel || !channel.isTextBased()) {
+    return;
+  }
+
+  const message = await channel.messages.fetch(data.id);
+
+  if (!message || message.author.bot || !message.guild) {
+    return;
+  }
+
+  /*if (client.permlevel(message, message.member!) >= 2) {
+    return;
+  }*/
+
+  if (!client.settings.has(message.guild.id)) {
+    const s = await SettingsModel.findOneAndUpdate(
+      { _id: message.guild.id },
+      { toUpdate: true },
+      {
+        upsert: true,
+        setDefaultsOnInsert: true,
+        new: true,
+      }
+    )
+      .populate("mentorRoles")
+      .populate("commands");
+
+    log.debug("Setting sync", {
+      action: "Fetch",
+      message: `Database -> Client (${message.guild.id})`,
+    });
+
+    message.settings = s;
+  } else {
+    const s = client.settings.get(message.guild.id);
+
+    if (!s) {
+      return;
+    }
+
+    message.settings = s;
+  }
+
+  if (!message.settings.pollsAllowed.includes(channel.id)) {
+    await message.reply("You are not allowed to send polls in this channel.");
+    await message.delete();
+  }
+};
---
+++ src/interactions/chatInput/polls.ts
@@ -0,0 +1,136 @@
+import {
+  ApplicationCommandOptionType,
+  CacheType,
+  ChannelType,
+  ChatInputCommandInteraction,
+  CommandInteraction,
+  EmbedBuilder,
+} from "discord.js";
+import {
+  CustomInteractionReplyOptions,
+  InteractionCommand,
+} from "../../classes/CustomInteraction";
+
+import { ApplicationCommandType } from "discord-api-types/v10";
+import { CustomClient } from "lib/client";
+import { SettingsModel } from "models/Settings";
+
+export default class SlashCommand extends InteractionCommand {
+  constructor(client: CustomClient) {
+    super(client, {
+      type: ApplicationCommandType.ChatInput,
+      name: "polls",
+      description: "Enable/disable polls in channels.",
+      options: [
+        {
+          name: "enable",
+          description: "Enable polls in a channel.",
+          type: ApplicationCommandOptionType.Subcommand,
+          options: [
+            {
+              name: "channel",
+              description: "The channel.",
+              required: true,
+              type: ApplicationCommandOptionType.Channel,
+              channelTypes: [
+                ChannelType.GuildText,
+                ChannelType.GuildVoice,
+                ChannelType.GuildAnnouncement,
+              ],
+            },
+          ],
+        },
+        {
+          name: "disable",
+          description: "Disable polls in a channel.",
+          type: ApplicationCommandOptionType.Subcommand,
+          options: [
+            {
+              name: "channel",
+              description: "The channel.",
+              required: true,
+              type: ApplicationCommandOptionType.Channel,
+              channelTypes: [
+                ChannelType.GuildText,
+                ChannelType.GuildVoice,
+                ChannelType.GuildAnnouncement,
+              ],
+            },
+          ],
+        },
+      ],
+      defaultMemberPermissions: "Administrator",
+    });
+  }
+
+  async execute(
+    interaction: CommandInteraction<CacheType>
+  ): Promise<CustomInteractionReplyOptions> {
+    if (!(interaction instanceof ChatInputCommandInteraction)) {
+      return { content: "Internal Error", eph: true };
+    }
+
+    const permLevel = this.client.permlevel(undefined, interaction.member);
+
+    if (permLevel < 4) {
+      return { content: "Insufficient Permissions", eph: true };
+    }
+
+    const subcmd = interaction.options.getSubcommand();
+    const channel = interaction.options.getChannel("channel", true, [
+      ChannelType.GuildText,
+      ChannelType.GuildVoice,
+      ChannelType.GuildAnnouncement,
+    ]);
+
+    if (subcmd == "enable") {
+      if (!interaction.settings.pollsAllowed.includes(channel.id)) {
+        this.client.settings.set(
+          interaction.settings._id,
+          await SettingsModel.findOneAndUpdate(
+            { _id: interaction.settings._id },
+            { $push: { pollsAllowed: channel.id }, toUpdate: true },
+            { upsert: true, setDefaultsOnInsert: true, new: true }
+          )
+        );
+
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(`Enabled polls in ${channel}`)
+              .setColor("Green"),
+          ],
+        });
+      } else {
+        await interaction.reply({
+          embeds: [this.client.errEmb(2, `Polls are already enabled in ${channel}`)],
+        });
+      }
+    } else if (subcmd == "disable") {
+      if (interaction.settings.pollsAllowed.includes(channel.id)) {
+        this.client.settings.set(
+          interaction.settings._id,
+          await SettingsModel.findOneAndUpdate(
+            { _id: interaction.settings._id },
+            { $pull: { pollsAllowed: channel.id }, toUpdate: true },
+            { upsert: true, setDefaultsOnInsert: true, new: true }
+          )
+        );
+
+        await interaction.reply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(`Disabled polls in ${channel}`)
+              .setColor("Green"),
+          ],
+        });
+      } else {
+        await interaction.reply({
+          embeds: [this.client.errEmb(2, `Polls are already disabled in ${channel}`)],
+        });
+      }
+    }
+
+    return {};
+  }
+}
