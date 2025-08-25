fadfe7cd763310ae4048b004cac609aab7262d62
Conrad306 <theoriesforlife@gmail.com>
2025-08-24 21:28:28 -0400

    Minor change on soon-to-deprecate event name, and match all valid discord invites

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -54,14 +54,11 @@ export default class MessageCreate extends EventLoader {
           "Internal error: data does not exist inside automod message;",
         );
       }
-      if (
-        content.match(/discord\.gg\/([a-zA-Z0-9]+)/g) ||
-        content.match(/discord\.com\/invite\/([a-zA-Z0-9]+)/g)
-      ) {
-        const matches = [
-          ...content.matchAll(/discord\.gg\/([a-zA-Z0-9]+)/g),
-          ...content.matchAll(/discord\.com\/invite\/([a-zA-Z0-9]+)/g),
-        ];
+
+      const DISCORD_INVITE_PATTERNS =
+        /(?:https?:\/\/)?(?:discord\.gg|discord(?:app)?\.com\/invite)\/([\w-]+)/gi;
+      if (content.match(DISCORD_INVITE_PATTERNS)) {
+        const matches = [...content.matchAll(DISCORD_INVITE_PATTERNS)];
         matches.forEach(async (match) => {
           const code = match[1];
           try {
